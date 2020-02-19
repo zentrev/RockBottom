9f5db10caf382c4bc90d440ea20c866af867ddfe
Kali <c.t.raines.iii@gmail.com>
2020-02-18 19:39:14 -0700

    Changes to ragdoll

---
+++ Assets/Scripts/Enemys/ActivateRagDoll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateRagDoll : MonoBehaviour
+{
+    #region Serialize Feilds
+    [SerializeField] Collider m_navCollider = null;
+    [SerializeField] Rigidbody m_holdingPin = null;
+    #endregion
+
+    public bool trigger = false;
+
+    private void Update()
+    {
+        if(trigger)
+        {
+            trigger = false;
+            OnRagdoll();
+        }
+    }
+
+    void OnRagdoll()
+    {
+        m_navCollider.enabled = false;
+        m_holdingPin.isKinematic = false;
+    }
+}
