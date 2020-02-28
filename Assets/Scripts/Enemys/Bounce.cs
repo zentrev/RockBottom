2e4ea96eab58788d7985b4c76965ffc4629c07a0
Sebastian Ryther <SRyther@student.neumont.edu>
2020-02-27 19:25:40 -0700

    Added basic (broken) bounce script

---
+++ Assets/Scripts/Enemys/Bounce.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    public Rigidbody rb;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (rb == null)
+        {
+            Debug.Log("No RigidBody connected.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        float newY = Mathf.Sin(timer);
+        Debug.Log(newY);
+        rb.position.Set(rb.position.x, rb.position.y + newY, rb.position.z);
+        //rb.MovePosition(new Vector3(rb.position.x, rb.position.y + newY, rb.position.z));
+    }
+}
