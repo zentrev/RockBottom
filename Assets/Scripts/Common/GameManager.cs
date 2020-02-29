41b9e5743d89ebbe8419a2230e38065ccb0bad93
Kaedian <38064751+Kaedian-Maxfield@users.noreply.github.com>
2020-02-28 18:45:52 -0700

    Rocks
    Spawn them. Throw them. watch them dissapear. But don't slide down a rocky slope.

---
+++ Assets/Scripts/Common/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public int gold = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int getGold()
+    {
+        return gold;
+    }
+
+    public void setGold(int value)
+    {
+        gold += value;
+    }
+
+    
+}
---
+++ Assets/Scripts/UpgradeShop/Shop.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField] List<GameObject> shopItems = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(GameObject interactable in shopItems)
+        {
+            interactable.TryGetComponent<Rigidbody>(out Rigidbody rigidbody);
+            interactable.TryGetComponent<BoxCollider>(out BoxCollider boxy);
+            if(interactable.TryGetComponent<Purchasable>(out Purchasable item))
+            {
+                if(GameManager.Instance.gold >= item.price)
+                {
+                    
+                    rigidbody.isKinematic = false;
+                    boxy.enabled = true;
+                }
+                else
+                {
+                    rigidbody.isKinematic = true;
+                    boxy.enabled = false;
+                }
+            }
+        }
+    }
+
+    public void PurchaseItem(Purchasable item)
+    {
+        GameManager.Instance.gold -= item.price;
+        item.price = 0;
+    }
+}
