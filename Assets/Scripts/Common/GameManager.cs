cc2442b8a09dd60f8f56bbd959985794116e7d30
QuickCoyote <36782326+QuickCoyote@users.noreply.github.com>
2020-03-05 19:57:08 -0700

    updat

--- Assets/Scripts/Common/GameManager.cs
+++ Assets/Scripts/Common/GameManager.cs
@@ -5,27 +5,5 @@ using UnityEngine;
 public class GameManager : Singleton<GameManager>
 {
     public int gold = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public int GetGold()
-    {
-        return gold;
-    }
-
-    public void AddGold(int value)
-    {
-        gold += value;
-    }
-
-    
+    public GameObject Face = null;
 }
