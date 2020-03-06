59fd3d7c2a4ab23d887af712b290df3442c77852
Sebastian Ryther <SRyther@student.neumont.edu>
2020-03-05 19:59:41 -0700

    Radius damage script

---
+++ Assets/Scripts/Items/Ammo/RadiusDamage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class RadiusDamage : MonoBehaviour
+{
+    [SerializeField] private float tickDamage = 1.0f;
+    private SphereCollider bounds;
+
+    private void Start()
+    {
+        this.bounds = this.GetComponent<SphereCollider>();
+        this.bounds.isTrigger = true;
+    }
+
+    private void Update()
+    {
+        Collider[] colliders = Physics.OverlapSphere(bounds.transform.position, bounds.radius);
+
+        foreach (Collider collider in colliders)
+        {
+            Damagable damagable = collider.GetComponent<Damagable>();
+
+            if (damagable)
+            {
+                damagable.ChangeHealth(this.tickDamage);
+            }
+        }
+    }
+}
