242e6246dd55e94dc5caace28aa16fa8d7d3ac37
Kaedian <38064751+Kaedian-Maxfield@users.noreply.github.com>
2020-03-11 15:29:36 -0600

    Fire Arrows!
    YEEESSSS!!!!! :)

---
+++ Assets/Scripts/Misc/SetRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.rotation = new Quaternion(0.5f, -0.5f, 0.5f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
